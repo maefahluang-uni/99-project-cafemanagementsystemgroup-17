102e911e1ef6df523a9dc367d422f25eca15413b
poonyawat0931 <6531503055@lamduan.mfu.ac.th>
2023-11-09 06:10:00 +0700

    90% login can use

--- src/main/java/th/mfu/SecurityConfig.java
+++ src/main/java/th/mfu/SecurityConfig.java
@@ -1,41 +1,42 @@
-// package th.mfu;
+package th.mfu;
 
-// import static org.springframework.security.config.Customizer.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("kuy")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
 
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsService() {
-//         UserDetails admin = User.withDefaultPasswordEncoder()
-//                 .username("admin")
-//                 .password("password")
-//                 .roles("ADMIN")
-//                 .build();
-//         return new InMemoryUserDetailsManager(admin);
-//     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeRequests(authorize -> {
+                authorize.antMatchers("/").permitAll();
+                authorize.antMatchers("/admin").hasRole("ADMIN");
+            })
+            .httpBasic()
+            .and()
+            .formLogin()
+                .loginPage("/login") // Specify your custom login page URL
+                .permitAll(); // Allow all users to access the custom login page
 
-//     @Bean
-//     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//         return http
-//                 .csrf(csrf -> csrf.disable())
-//                 .authorizeRequests(auth -> {
-//                     auth.antMatchers("/").permitAll();
-//                     auth.antMatchers("/admin").hasRole("ADMIN");
-//                 })
-//                 .httpBasic(withDefaults())
-//                 .build();
-                
-//     }
-
-// }
+        return http.build();
+    }
+}
--- src/main/java/th/mfu/controller/LoginController.java
+++ src/main/java/th/mfu/controller/LoginController.java
@@ -1,21 +1,17 @@
 package th.mfu.controller;
 
-import java.security.Principal;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-    @GetMapping("/login")
-    public String login(Principal principal) {
-        if (principal != null) {
-            return "redirect:/admin";
-        }
-        return "/login";
-    }
+class LoginController {
+	@GetMapping("/login")
+	String login() {
+		return "login";
+	}
+}
     // @GetMapping("/login?logout")
     // public String logoutSuccess() {
     //     return "logout-success"; // You can define a custom logout success template
     // }
-}
+
