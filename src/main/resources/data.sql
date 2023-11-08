fbb4549dc6846807f3ebf416bb30c9acbb929652
Osathi <6531503093@lamduan.mfu.ac.th>
2023-11-08 17:37:28 +0700

    show picture finish

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,26 +5,26 @@ INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dis
 INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (4, 'Taiwanese Milk tea', 'tea', 'imageTaiwanese.png', 80,40);
 
 -- dessert
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (5, 'Croffle', 'dessert', 'pic', 50,35);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (6, 'Chese Cake', 'dessert', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (7, 'Chocolate Cake', 'dessert', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (8, 'Coconut Cake', 'dessert', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (9, 'Almond Cake', 'dessert', 'pic', 50,35);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (5, 'Croffle', 'dessert', 'Croffle.png', 50,35);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (6, 'Chese Cake', 'dessert', 'CheseCake.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (7, 'Chocolate Cake', 'dessert', 'ChocolateCake.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (8, 'Coconut Cake', 'dessert', 'Coconut Cake.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (9, 'Almond Croissant', 'dessert', 'AlmondCroissant.png', 50,35);
 
 -- coffee
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (10, 'Mocha', 'coffee', 'pic', 50,40);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (11, 'Americano Orange', 'coffee', 'pic', 50,60);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (12, 'Americano', 'coffee', 'pic', 50,40);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (13, 'Esspresso', 'coffee', 'pic', 50,40);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (14, 'Cappuccino', 'coffee', 'pic', 50,40);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (15, 'Macchiato', 'coffee', 'pic', 50,40);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (10, 'Mocha', 'coffee', 'Mocha.png', 50,40);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (11, 'Americano Orange', 'coffee', 'Americano Orange.png', 50,60);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (12, 'Americano', 'coffee', 'Americano.png', 50,40);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (13, 'Esspresso', 'coffee', 'Esspresso.png', 50,40);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (14, 'Cappuccino', 'coffee', 'Cappuccino.png', 50,40);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (15, 'Macchiato', 'coffee', 'Macchiato.png', 50,40);
 
 --smoothie
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (16, 'Mixed Berry Smoothie', 'smoothie', 'pic', 50,65);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (17, 'Blueberry Smoothie', 'smoothie', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (18, 'Strawberry Smoothie', 'smoothie', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (19, 'Mango Smoothie', 'smoothie', 'pic', 50,55);
-INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (20, 'Peach Smoothie', 'smoothie', 'pic', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (16, 'Mixed Berry Smoothie', 'smoothie', 'Mixed Berry Smoothie.png', 50,65);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (17, 'Blueberry Smoothie', 'smoothie', 'Blueberry Smoothie.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (18, 'Strawberry Smoothie', 'smoothie', 'Strawberry Smoothie.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (19, 'Mango Smoothie', 'smoothie', 'Mango Smoothie.png', 50,55);
+INSERT IGNORE INTO Dishes (id, dish_name, dishtype, dish_picture, dish_stock,dish_price) VALUES (20, 'Peach Smoothie', 'smoothie', 'Peach Smoothie.png', 50,55);
 
 --mat
 INSERT IGNORE INTO Material (id, mat_name, mat_amount) VALUES (1, 'milk', 20);
